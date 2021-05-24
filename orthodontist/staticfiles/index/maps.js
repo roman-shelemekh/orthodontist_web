04cc20a9e352bcd4281a833eec49242ac53be854
Roman <shelemekh@tut.by>
2021-05-24 08:21:23 +0800

    cleaned

--- orthodontist/staticfiles/index/maps.js
+++ orthodontist/staticfiles/index/maps.js
@@ -14,7 +14,6 @@ function init() {
         headers: {'X-Requested-With': 'XMLHttpRequest'}
     }).then(response => response.json()).then(json => {
         for(let clinic in json){
-            console.log(json[clinic])
             myMap.geoObjects
                 .add(new ymaps.Placemark([parseFloat(json[clinic].latitude), parseFloat(json[clinic].longitude)], {
                     balloonContent: '<b>' + json[clinic].name + '</b></br>Адрес: ' + json[clinic].address + '</br>Телефон: ' + json[clinic].phone_number1 + ', ' + json[clinic].phone_number2,
@@ -24,18 +23,6 @@ function init() {
                 }))
         }
     })
-
-
-
-
-    myMap.geoObjects
-        .add(new ymaps.Placemark([45.041687, 38.954787], {
-            balloonContent: '<b>ДЕНТиК на Тургенева</b></br>Адрес: ул. Тургенева, 23</br>Телефон:',
-            iconCaption: 'ДЕНТиК на Тургенева'
-        }, {
-            preset: 'islands#blueMedicalIcon'
-        }))
-
 }
 
 
